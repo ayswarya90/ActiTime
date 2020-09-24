e8c651e9650135b84e921aee27568c8b11c9eea7
vijay <vijay@192.168.0.16>
2020-09-23 19:28:21 -0700

    New file added

---
+++ src/test/java/com/scripts/Actitime/TestActitimeTasks.java
@@ -0,0 +1,13 @@
+package com.scripts.Actitime;
+
+import org.testng.annotations.Test;
+
+public class TestActitimeTasks {
+	@Test
+	
+	public void output()
+	{
+		System.out.println("New Class");
+	}
+
+}
